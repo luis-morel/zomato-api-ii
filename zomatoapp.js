1dd1b0382924f4a11d307a51fc08457ef3daa901
LMorel77 <lmorel77@aol.com>
2020-06-17 21:01:51 -0400

    Updates

--- zomatoapp.js
+++ zomatoapp.js
@@ -1,185 +1,172 @@
 $(document).ready(function () {
 
-    const apiKey = 'ef5b1abf524ce5d1f47b5b0f797a9d72';
-    const citySearchURL = "https://developers.zomato.com/api/v2.1/cities";
-    const cuisineSearchURL = "https://developers.zomato.com/api/v2.1/cuisines";
-    const searchURL = "https://developers.zomato.com/api/v2.1/search";
-    var apiData;
-    var cuisine;
-    var cuisineId;
-    var city;
-    var cityId;
+    const apiKey = 'ef5b1abf524ce5d1f47b5b0f797a9d72',
+        citySearchURL = 'https://developers.zomato.com/api/v2.1/cities',
+        cuisineSearchURL = 'https://developers.zomato.com/api/v2.1/cuisines',
+        searchURL = 'https://developers.zomato.com/api/v2.1/search';
+    let cityId = null,
+        cityName = null,
+        cuisineId = null,
+        cuisineName = null;
 
-    // Reset button event handler
-    $(document).on("click", "#resetButton", function(event) {
-        
-        event.preventDefault();
-        $("#head").empty();
-        $("#hits").empty();
-        $("#city-input").empty();
-        $("#city-input").val("");
-        $("#cuisine-input").empty();
-        $("#cuisine-input").css("display", "none");
-        
-    }); // End of reset button event handler
+    // Clear City Datalist
+    const clearCityDatalist = () => {
 
-    // City search event handler
-    $(document).on("click", ".api-call", function (event) {
+        $('#cities').empty();
+        let cities = $('#cities').children().length
+        if (cities) return false
+        else return true;
 
-        event.preventDefault();
-        $("#hits").empty();
-        city = $("#city-input").val().trim().replace(/\s+/g, ''); // Remove ALL spaces
+    };
 
-        // Console logging city input
-        console.log(`City: ${city}`);
+    // Reset Search Tool Data
+    const clearSearchToolData = (event) => {
 
-        // Ajax call for list of matching cities
-        $.ajax({
-            type: "GET",
-            headers: {  'X-Zomato-API-Key': apiKey  }, //only allowed non-standard header
-            url: citySearchURL,
-            dataType: 'json',
-            data: { q: city },
-            processData: true, // Converts data to query string
-            success: function (data) {
-                console.log(`Inside Ajax.success!`);
-                // let message = $("<span>");
-                // message.text("Select Your City:");
-                // $("#head").html(message);
-                $("#head").html("Select Your City:");
-                if (data.location_suggestions.length === 0) {
-                    $("#hits").html("<p>Unable to locate specified city name. Please check your spelling and try again.</p>");
-                    return;
-                }
-                else {
-                    apiData = data;
-                    if ($("#cuisine-input option:selected").val() === "null") {
-                        for (let i = 0; i < data.location_suggestions.length; i++) {
-                            var button = $("<button>");
-                            button.text(data.location_suggestions[i].name);
-                            button.val(data.location_suggestions[i].name);
-                            button.attr('id', i);
-                            button.attr('class', 'buttons cities');
-                            $("#hits").append(button);
+        $('#cities').empty();
+        $('#cuisines').empty();
+        $('#search-header').empty();
+        $('#search-data').empty();
+        $('#city-input').val('');
+        $('#cuisines').val('');
+        $('datalist').css('display', 'none');
+        $('#cuisine-selection').css('display', 'none');
+        $('#search-tool-reset').css('display', 'none');
+
+    }
+
+    // Zomato API - City Search
+    const handleCitySearch = (event) => {
+
+        if (event.key != undefined) {
+            let cityInput = $('#city-input').val().trim();
+            $.ajax({
+                type: 'GET',
+                headers: { 'X-Zomato-API-Key': apiKey },
+                url: citySearchURL,
+                dataType: 'json',
+                data: { q: cityInput },
+                processData: true, // Converts data to query string
+                success: (data) => {
+                    console.log('Zomato - City Data', data);
+                    let cities = data.location_suggestions;
+                    cities.sort((a, b) => (a.name > b.name) ? 1 : -1)
+                    if (clearCityDatalist())
+                        for (let i = 0; i < cities.length; i++) {
+                            let option = $('<option>');
+                            option.val(cities[i].name);
+                            option.text(cities[i].name);
+                            option.attr('data-cityid', cities[i].id);
+                            $('#cities').append(option);
                         }
-                    }
                 }
-            }
-        }); // End of Ajax call for list of matching cities
+            });
+        };
 
-    }); // End of city search event handler
+    };
 
-    // City selection event handler
-    $(document).on("click", ".cities", function (event) {
+    // Zomato API - Cuisine Search
+    const handleCuisineSearch = (event) => {
 
-        event.preventDefault();
-        $("#head").empty();
-        $("#hits").empty();
-        $("#city-input").val($(this).val());
-        $("#cuisine-input").css("display", "inline");
-        const index = $(this).attr('id');
-        cityId = apiData.location_suggestions[index].id;
-
-        // Ajax call for list of available cuisines
+        cityName = $('#city-input').val();
+        let cityDataList = $('datalist#cities').children();
+        for (let i = 0; i < cityDataList.length; i++) {
+            let optionVal = $(cityDataList[i]).val();
+            if (optionVal === cityName) {
+                cityId = $(cityDataList[i]).attr('data-cityid');
+                break;
+            };
+        };
         $.ajax({
-            type: "GET",
-            headers: {  'X-Zomato-API-Key': apiKey  }, //only allowed non-standard header
+            type: 'GET',
+            headers: { 'X-Zomato-API-Key': apiKey }, //only allowed non-standard header
             url: cuisineSearchURL,
             dataType: 'json',
             data: { city_id: cityId },
             processData: true, // Converts data to query string
             success: function (data) {
-                if (data.cuisines.length === 0) {
-                    $("#hits").html("<p>Unable to locate restaurants serving " + cuisine + ". Please check your spelling and try again, or try a different cuisine and/or city.</p>");
-                    return;
-                }
+                console.log('Zomato - Cuisine Data', data);
+                if (data.cuisines.length === 0)
+                    $('#search-data').html(`<p>Unable to locate restaurants serving ${cuisine}. Please try another cuisine.</p>`);
                 else {
                     for (let i = 0; i < data.cuisines.length; i++) {
-                        var option = $("<option>");
-                        option.val(data.cuisines[i].cuisine.cuisine_name);
-                        option.text(data.cuisines[i].cuisine.cuisine_name);
-                        option.attr("data-cuisineId", data.cuisines[i].cuisine.cuisine_id);
-                        $("#cuisine-input").append(option);
+                        let cuisine = data.cuisines[i].cuisine.cuisine_name,
+                            id = data.cuisines[i].cuisine.cuisine_id,
+                            option = $('<option>');
+                        option.val(cuisine).text(cuisine);
+                        option.attr('data-cuisineId', id);
+                        $('#cuisines').append(option);
                     }
-                }
+                    $('#cuisine-selection').css('display', 'block');
+                };
             }
-        }); // End of Ajax call for list of available cuisines
+        });
 
-    }); // End of city selection event handler
+    };
 
-    // Cuisine selection event handler
-    $("#cuisine-input").on("change", function (event) {
+    // Zomato API - Restaurant Search
+    const handleRestaurantSearch = (event) => {
 
-        event.preventDefault();
-        cuisine = $("#cuisine-input option:selected").val();
-        city = $("#city-input").val();
-        var message = $("<span>");
-        message.text(cuisine + " Restaurants in " + city);
-        $("#head").html(message);
-        cuisineId = $("#cuisine-input option:selected").attr("data-cuisineId");
-
-        // Ajax call for list of restaurants
+        cuisineName = $('#cuisines option:selected').val();
+        cuisineId = $('#cuisines option:selected').attr('data-cuisineId');
+        let restaurantSearchUrl = `${searchURL}?entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}`;
         $.ajax({
-            type: "GET",
-            headers: {  'X-Zomato-API-Key': apiKey  }, //only allowed non-standard header
-            url: searchURL,
+            type: 'GET',
+            headers: { 'user-key': apiKey },
+            url: restaurantSearchUrl,
             dataType: 'json',
-            data: {
-                cuisines: cuisineId,
-                entity_id: cityId,
-                entity_type: 'city'
-            },
-            processData: true, // Converts data to query string
             success: function (data) {
-                $("#hits").empty();
-                if (data.restaurants.length === 0) {
-                    $("#hits").html("<p>Unable to locate restaurants serving " + cuisine + ". Please check your spelling and try again, or try a different cuisine and/or city.</p>");
-                    return;
-                }
-                else if ($("#cuisine-input option:selected").val() === "null") {
-                    return;
-                }
+                console.log('Zomato - Restaurant Data', data);
+                let results = data.restaurants;
+                $('#search-data #search-header').empty();
+                if (results.length === 0)
+                    $('#search-data').html(`<p>Unable to locate restaurants serving ${cuisineName}. Please try a different cuisine and/or city.</p>`);
+                else if (cuisineName === 'null' || cuisineName === '')
+                    $('#search-data').html(`<p>Please select a cuisine.</p>`);
                 else {
-                    // Displaying restaurant data
-                    // Venue Info: Name, City, Address, cuisines, cost, and Zomato URL
-                    for (let i = 0; i < data.restaurants.length; i++) {
-
-                        var anchor = $("<a>");
-                        var mainDiv = $("<div>");
-                        var p = $("<p>");
-                        var priceRange = data.restaurants[i].restaurant.price_range;
-                        var span = $("<span>");
-                        var subDiv1 = $("<div>");
-                        var subDiv2 = $("<div>");
-                        var subDiv3 = $("<div>");
-
-                        subDiv1.append(data.restaurants[i].restaurant.name);
-                        subDiv1.addClass("venueName");
-                        subDiv2.append(data.restaurants[i].restaurant.location.city);
-                        subDiv2.addClass("venueCity");
-                        subDiv3.append(data.restaurants[i].restaurant.location.address + "<hr>");
-                        p.append("Cuisines: " + data.restaurants[i].restaurant.cuisines + "<br>");
-                        span.addClass("venueCost");
-                        for (var cost = 0; cost < priceRange; cost++) span.append("$");
-                        p.append("Cost: ", span);
-                        anchor.attr("href", data.restaurants[i].restaurant.url);
-                        anchor.attr("target", "_blank");
-                        anchor.text("Visit Zomato Restaurant Page");
-                        p.append("<br>");
+                    // Display Restaurant Data (e.g. Venue Name, City, Address, etc.)
+                    let message = $('<span>').text(`${cuisineName} Restaurants in ${cityName}`);
+                    $('#search-header').html(message);
+                    for (let i = 0; i < results.length; i++) {
+                        var anchor = $('<a>');
+                        var mainDiv = $('<div>');
+                        var p = $('<p>');
+                        var priceRange = results[i].restaurant.price_range;
+                        var span = $('<span>');
+                        var subDiv1 = $('<div>');
+                        var subDiv2 = $('<div>');
+                        var subDiv3 = $('<div>');
+                        subDiv1.append(results[i].restaurant.name);
+                        subDiv1.addClass('venueName');
+                        subDiv2.append(results[i].restaurant.location.city);
+                        subDiv2.addClass('venueCity');
+                        subDiv3.append(results[i].restaurant.location.address + '<hr>');
+                        p.append('Cuisines: ' + results[i].restaurant.cuisines + '<br>');
+                        span.addClass('venueCost');
+                        for (var cost = 0; cost < priceRange; cost++) span.append('$');
+                        p.append('Cost: ', span);
+                        anchor.attr('href', results[i].restaurant.url);
+                        anchor.attr('target', '_blank');
+                        anchor.text('Visit Zomato Restaurant Page');
+                        p.append('<br>');
                         p.append(anchor);
                         subDiv3.append(p);
                         mainDiv.append(subDiv1);
                         mainDiv.append(subDiv2);
                         mainDiv.append(subDiv3);
-                        mainDiv.addClass("venues");
-                        $("#hits").append(mainDiv);
-
-                    }
+                        mainDiv.addClass('venues');
+                        $('#search-data').append(mainDiv);
+                    };
+                    $('#search-tool-reset').css('display', 'block');
                 }
             }
+        });
 
-        }); // End of Ajax call for list of restaurants
+    };
 
-    }); // End of cuisine input event handler
+    // Event Handlers
+    $('#city-input').keyup(handleCitySearch);
+    $('#api-cuisine-search').click(handleCuisineSearch);
+    $('#api-restaurant-search').click(handleRestaurantSearch);
+    $('#clear-search-data').click(clearSearchToolData);
 
-}); // End of ready() event handler
+});
